70efd5e880ad8e57cc58db56d8523c315a508219
iitwistii <ali_kashash@hotmail.co.uk>
2024-09-21 21:42:45 +0200

    set square size to dynamically change and fit in container

--- javascript.js
+++ javascript.js
@@ -4,21 +4,21 @@ const container = document.querySelector("#container")
 const button = document.querySelector("#prompt")
 button.addEventListener("click", () => {
     container.innerHTML = "";
-    newGrid = Number(window.prompt("How many squares per side?"));
-    console.log(`You asked for ${newGrid} amount of squares per line`)
-
-    for (let i = 0; i < newGrid; i++) {
-        for (let i = 0; i < newGrid; i++) {
-            const square = document.createElement("div")
+    numberOfSquares = Number(window.prompt("How many squares per side?"));
+    const squareSize = (960 / numberOfSquares);
+    console.log(squareSize)
+    for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
+        const square = document.createElement("div")
             square.classList.add("square")
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
             container.appendChild(square)
-            square.addEventListener("mouseenter", () => {
-            square.classList.add("color")
-            console.log("mouse on")
-            })
-        }
+        square.addEventListener("mouseenter", () => {
+        square.classList.add("color")
+        })
     }
-})
+    }
+)
 
 
 
