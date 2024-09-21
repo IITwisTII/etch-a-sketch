40f70acff35f96e31f7d6adaa8456c7188602a41
iitwistii <ali_kashash@hotmail.co.uk>
2024-09-21 20:41:53 +0200

    save progress on trying to make the grid work

--- javascript.js
+++ javascript.js
@@ -1,16 +1,27 @@
 const container = document.querySelector("#container")
 
-for (let i = 0; i < 16; i++) {
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement("div")
-        square.classList.add("square")
-        container.appendChild(square)
-        square.addEventListener("mouseenter", () => {
-        square.classList.add("color")
-        console.log("mouse on")
-        })
+
+const button = document.querySelector("#prompt")
+button.addEventListener("click", () => {
+    container.innerHTML = "";
+    newGrid = Number(window.prompt("How many squares per side?"));
+    console.log(`You asked for ${newGrid} amount of squares per line`)
+
+    for (let i = 0; i < newGrid; i++) {
+        for (let i = 0; i < newGrid; i++) {
+            const square = document.createElement("div")
+            square.classList.add("square")
+            container.appendChild(square)
+            square.addEventListener("mouseenter", () => {
+            square.classList.add("color")
+            console.log("mouse on")
+            })
+        }
     }
-}
+})
+
+
+
 
 
 
